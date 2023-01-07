6f548621b22c6f894e2705e40273cba091738a83
Dominik Haeusser <haeusser-dominik@web.de>
2023-01-07 15:26:45 +0100

    implement basic connection

--- website_connector/main.py
+++ website_connector/main.py
@@ -1,36 +1,55 @@
+import time
+from logging import getLogger
+
 import paho.mqtt.client as mqtt
+
+BROKER_IP = "192.168.62.168"
+
+logger = getLogger('mqttInput')
+logger.setLevel(mqtt.MQTT_LOG_INFO)
+
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    print(f'Connected with result code {rc} to {BROKER_IP}')
+    client.subscribe('led_matrix/#', qos=1)
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     # client.subscribe("$SYS/#")
     # client.subscribe("led")
 
+
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    print(f'{msg.topic} {msg.payload}')
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
 
-client.connect("192.168.188.168", 1884, 60)
-client.publish('led_matrix', 'subscribe')
+def on_log(client, userdata, level, buf):
+    print(f'log: {buf}')
 
-while True:
-    print('1 - Restart LED Matrix')
-    print('q - Exit')
-    user_input = input('Input: ')
-    if user_input == '1':
-        client.publish('led_matrix', 'restart')
-    elif user_input == 'q':
-        exit()
 
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
-client.loop_forever()
+def main():
+    client = mqtt.Client()
+    client.enable_logger(logger=logger)
+
+    # callbacks
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.on_log = on_log
+
+    client.connect_async(BROKER_IP, 1884, 10)
+    client.loop_start()
+    
+    while True:
+        print('1 - Restart LED Matrix')
+        print('q - Exit')
+        user_input = input('Input: ')
+        if user_input == '1':
+            client.publish('led_matrix', 'restart')
+        elif user_input == 'q':
+            exit()
+
+    
+if __name__ == '__main__':
+    main()
