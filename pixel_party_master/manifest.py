2c0f8b421d15b588031c3d1a37ac498bd9191fb5
domroon <haeusser-dominik@web.de>
2023-02-23 18:07:34 +0100

    clean up whole project

--- pixel_party_master/manifest.py
+++
@@ -1,8 +0,0 @@
-# Include the board's default manifest
-include("$(PORT_DIR)/boards/manifest.py")
-
-# add my modules that should be freezed in firmware
-module("networking.py")
-module("i2c_lcd.py") # find another one - no licens
-module("lcd_api.py") # find another one - no license
-module("ulogging.py")
