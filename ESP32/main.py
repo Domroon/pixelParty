0781a665cb4b1515464c87235c469ac95e73c1b4
domroon <haeusser-dominik@web.de>
2023-02-23 20:16:38 +0100

    implement Device class

--- ESP32/main.py
+++ ESP32/main.py
@@ -5,9 +5,15 @@ from random import randint
 import gc
 
 
+MATRIX_PIN = 33
+UART_TX_PIN = 12
+UART_RX_PIN = 14
+IRQ_PIN = 35
+
+
 class Matrix:
     def __init__(self):
-        self.pin = Pin(33, Pin.OUT)
+        self.pin = Pin(MATRIX_PIN, Pin.OUT)
         self.np = NeoPixel(self.pin, 1024)
         self.pixels = []
 
@@ -29,7 +35,7 @@ class Matrix:
                         pixel_list.append(int(value))
                     self.pixels.append(pixel_list)
 
-    def read_pixels_from_string_list(self, string_list):
+    def read_pixels(self, string_list):
         pixels_string = []
         for line in string_list:
             pixels_string.append(line.decode().rstrip())  
@@ -74,8 +80,9 @@ class Animation:
 
 class Receiver:
     def __init__(self):
-        self.uart = UART(1, baudrate=115200, tx=12, rx=14)
-        self.lines = []
+        self.uart = UART(1, baudrate=115200, tx=UART_TX_PIN, rx=UART_RX_PIN)
+        self.command = None
+        self.data = []
         
     def receive(self):
         while True:
@@ -85,22 +92,66 @@ class Receiver:
             time.sleep(0.03)
         line = line.decode()
         if line == 'PIXELS':
+            self.command = 'PIXELS'
             self._receive_pixels_data()
         elif line == 'ANI':
+            self.command = 'ANI'
             self._receive_ani_data()
         else:
             raise Exception('Unknown Command from UART')
     
     def _receive_pixels_data(self):
-        self.lines.clear()
+        self.data.clear()
         while True:
             line = self.uart.readline()
             if line:
                 # print(line)
-                self.lines.append(line)
+                self.data.append(line)
             if line == b'EOF':
                 break
-        self.lines.pop()
+        self.data.pop()
     
     def _receive_ani_data(self):
         print('It should now receive the animation type and parameters for that type')
+
+
+class Device:
+    def __init__(self):
+        self.matrix = Matrix()
+        self.ani = Animation()
+        self.rec = Receiver()
+        self.irq_btn = Pin(IRQ_PIN, Pin.IN)
+        self.mode = None
+        self.run_loops = False
+
+    def _listen_uart(self):
+        self.rec.receive()
+        self.mode = self.rec.command
+        self.data = self.rec.data
+        self.run_loops = False
+        self._show_on_matrix()
+
+    def _show_on_matrix(self):
+        self.matrix.clear()
+        if self.mode == 'PIXELS':
+            self.matrix.read_pixels(self.data)
+            self.matrix.write_to_led()
+        elif self.mode == 'ANI':
+            self.run_loops = True
+            while True:
+                if not self.run_loops:
+                    break
+                print('Show Animation')
+                time.sleep(1)
+
+    def start(self):
+        self.irq_btn.irq(self._listen_uart)
+
+
+def main():
+    device = Device()
+    device.start()
+
+
+if __name__ == '__main__':
+    main()
